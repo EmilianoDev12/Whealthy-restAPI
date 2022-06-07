90631044b798cdc5ec53e1aca152dcf6b1efffdb
EmilianoDev12 <gcasillasa1900@alumno.ipn.mx>
2022-06-07 01:22:10 -0500

    nueva ruta

--- src/index.js
+++ src/index.js
@@ -27,6 +27,15 @@ app.use('/api/pacientes', require('./routes/pacientes'));
 app.use('/api/citas',require('./routes/citas'));
 app.use('/api/recetas', require('./routes/recetas'));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
+
 app.post("/agendar", (req, res) => {
   const playload = req.body;
   const config = {
