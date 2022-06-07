81eb894828116a4a3976d92cb2387855377ef79a
EmilianoDev12 <gcasillasa1900@alumno.ipn.mx>
2022-06-07 01:49:03 -0500

    nueva ruta

--- src/index.js
+++ src/index.js
@@ -26,8 +26,6 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/pacientes', require('./routes/pacientes'));
 app.use('/api/citas',require('./routes/citas'));
 app.use('/api/recetas', require('./routes/recetas'));
-
-
 app.post("/agendar", (req, res) => {
   const playload = req.body;
   const config = {
@@ -79,6 +77,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 //Iniciar el servidor
